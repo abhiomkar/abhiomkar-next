b045a642116107f7692308aae470fd470de3c8dd
Abhinay Omkar <abhiomkar@gmail.com>
2018-10-17 15:12:33 -0400

    Integrated Google Analytics code

--- components/layout.js
+++ components/layout.js
@@ -6,6 +6,14 @@ const Layout = (props) => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <script async src='https://www.googletagmanager.com/gtag/js?id=UA-7271166-2'></script>
+      <script dangerouslySetInnerHTML={{__html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-7271166-2');`}} />
     </Head>
     <Header />
     <div className={`body-content ${props.fullWidth ? 'full-width' : ''}`}>
