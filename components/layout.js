3ee62196e2dcae708a027f4d2da9b75b96564492
Abhinay Omkar <abhiomkar@gmail.com>
2022-12-26 17:19:19 +0530

    Fix a11y issues. Score 100 on light house

--- components/layout.js
+++ components/layout.js
@@ -11,6 +11,7 @@ const gaAnalyticsTrackCode = `
 const Layout = (props) => (
   <div className='main-container'>
     <Head>
+      <title>Abhinay Omkar</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <link rel="icon" href="/static/favicon.ico" />
